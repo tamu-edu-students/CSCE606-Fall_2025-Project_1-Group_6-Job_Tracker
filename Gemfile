20b4b0f76a11e5ab7f7d7390a35ffc9473bd9058
Saswat Mishra <saswat.mishra@tamu.edu>
2025-09-22 05:29:47 -0500

    Added basic HTML and CSS styling for the landing page and signup page.

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ group :test do
   gem "capybara"
   gem "selenium-webdriver"
 end
+
+gem "bootstrap", "= 5.3.8"
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "landing#index"
 
   get "dashboard", to: "dashboard#index"
   get "personal_info", to: "dashboard#personal_info"
