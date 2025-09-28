def45416fcdc9b4021d68390c54ceafe64aea3cd
Malvika0108 <malvika.koushik@tamu.edu>
2025-09-28 14:30:25 -0500

    Created Job Dashboard - Kanban Board based on status of applications

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,11 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @jobs = Job.includes(:company).all
   end
 
   def personal_info
     @user = current_user
   end
+  
 end
