bc0047052ea402a26f52c360970ec62337dd1c30
Malvika0108 <malvika.koushik@tamu.edu>
2025-10-01 00:31:53 -0500

    Created a Job List View, integrated it into the Dashboard, and updated routes.

--- config/routes.rb
+++ config/routes.rb
@@ -5,28 +5,16 @@ Rails.application.routes.draw do
   get "jobs/index"
 
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   root to: "landing#index"
 
   get "dashboard", to: "dashboard#index"
   get "personal_info", to: "dashboard#personal_info"
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  resources :jobs, only: [:index]
-
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'my_jobs', to: 'jobs#index', as: 'my_jobs'
 
   resources :jobs
+  resources :jobs, only: [:index]
   resources :companies, only: [:index, :show, :new, :create]
 end
 
