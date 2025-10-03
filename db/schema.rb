33f8fe71987f9770b382f9d032cf932f2689a925
shivam141102 <shivam2002mishra@gmail.com>
2025-10-02 22:00:03 -0500

    Added latest schema.rb after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_28_060000) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_03_025828) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "website"
