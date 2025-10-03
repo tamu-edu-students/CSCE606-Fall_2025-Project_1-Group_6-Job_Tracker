500a22af15f9ba0ca7a67af1ad17a2d7fc372977
mihirsanjay <mihirsanjay@gmail.com>
2025-10-02 23:39:00 -0500

    Resolved errors with edit, add Job CRUD with validations, Company routes/views, and comprehensive RSpec tests

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "personal_info", to: "dashboard#personal_info"
 
   resources :jobs
+  resources :companies, only: [:index, :show, :new, :create]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
