48554df565a5c86940d3b228a61d88961f76704e
shivam141102 <shivam2002mishra@gmail.com>
2025-10-05 11:08:20 -0500

    Improvised CSS and frontend for edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # Permit only the necessary fields during sign up and account update
-    permitted_keys = [:full_name, :phone]
+    permitted_keys = [:full_name, :phone, :profile_photo]
 
     devise_parameter_sanitizer.permit(:sign_up, keys: permitted_keys)
     devise_parameter_sanitizer.permit(:account_update, keys: permitted_keys)
